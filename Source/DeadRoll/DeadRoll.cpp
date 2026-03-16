0846188661b0e67fa479f9fe15245c53a826458e
Xpertiise <123083909+Xpertiise@users.noreply.github.com>
2026-03-16 10:42:49 +0000

    Initial commit

---
+++ Source/DeadRoll/DeadRoll.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DeadRoll.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DeadRoll, "DeadRoll" );
