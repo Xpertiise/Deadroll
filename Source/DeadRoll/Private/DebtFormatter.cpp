24a36e5e199624fd298ed8bd00fe569e0f9b8e92
Xpertiise <123083909+Xpertiise@users.noreply.github.com>
2026-04-04 19:25:26 +0100

    displays

---
+++ Source/DeadRoll/Private/DebtFormatter.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DeadRoll/Public/DebtFormatter.h"
+
+FString UDebtFormatting::FormatDebtDisplay(int32 DebtAmount) {
+	if (DebtAmount < 1000000) {
+		return FString::Printf(TEXT("%06d"), DebtAmount);
+	} else {
+		return FString::Printf(TEXT("%.2e"), (float)DebtAmount);
+	}
+}
---
+++ Source/DeadRoll/Public/DebtFormatter.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "DebtFormatter.generated.h"
+
+UCLASS()
+class DEADROLL_API UDebtFormatting : public UBlueprintFunctionLibrary {
+	GENERATED_BODY()
+  
+public:
+	UFUNCTION(BlueprintCallable, Category = "Debt")
+	static FString FormatDebtDisplay(int32 DebtAmount);
+};
