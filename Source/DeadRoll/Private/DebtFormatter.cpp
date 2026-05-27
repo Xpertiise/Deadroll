e891f0b3d39797b75078743aac6118114c6fdd44
Xpertiise <123083909+Xpertiise@users.noreply.github.com>
2026-05-27 19:37:50 +0100

    test

--- Source/DeadRoll/Private/DebtFormatter.cpp
+++ Source/DeadRoll/Private/DebtFormatter.cpp
@@ -3,13 +3,12 @@
 
 #include "DeadRoll/Public/DebtFormatter.h"
 
-UFUNCTION(BlueprintCallable, Category = "Debt")
-static FString FormatDebtDisplay(int32 DebtAmount) {
+FString UDebtFormatting::FormatDebtDisplay(int32 DebtAmount) {
 	if (DebtAmount < 1000000) {
 		FString Formatted = FString::Printf(TEXT("%06d"), DebtAmount);
-		Formatted.InsertAt(3, TEXT(","));
 		return Formatted;
-	} else {
+	}
+	else {
 		return FString::Printf(TEXT("%.2e"), (float)DebtAmount);
 	}
 }
